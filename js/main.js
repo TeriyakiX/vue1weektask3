06d3bc336f2143a84d900a6dcf329744731b854c
Данил Демидов <gr404_dedde@lab-402-5.tomtit-tomsk.ru>
2023-03-02 12:02:14 +0700

    Накидал снова грязи

--- js/main.js
+++ js/main.js
@@ -48,7 +48,22 @@ Vue.component('notes', {
     computed: {},
 
     mounted() {
+        eventBus.$on('addColumn_1', card => {
+            this.note_list1.push(card)
+        })
+        eventBus.$on('addColumn_2', card => {
+            this.note_list2.push(card)
+        })
+        eventBus.$on('addColumn_3', card => {
+            this.note_list3.push(card)
+        })
+        eventBus.$on('addColumn_4', card => {
+            this.note_list4.push(card)
 
+            if (card.date > card.deadline) {
+                card.period = false
+            }
+        })
 
     },
     props: {
@@ -64,81 +79,79 @@ Vue.component('notes', {
 
 Vue.component('add-notes', {
     template: `
-
-       <form  @submit.prevent="onSubmit">
-                        <h1>Заметочки</h1>
-            <div class="note_name">
-            <input type="text" v-model="name" placeholder="Введите имя заметки">
-                  
-            </div>
-            <div class="note_1">
-                
-                        <input type="text"  v-model="field1"   placeholder="Введите 1 строку" >
-    
-                        <input type="text"   v-model="field2"  placeholder="Введите 2 строку">
-    
-                        <input type="text"   v-model="field3"  placeholder="Введите 3 строку">
-                        <br>
-                        
-                        <input type="text"  v-model="field4"   placeholder="Введите 4 строку" v-show ="note4">
-                        <br>
-                        
-                        <input type="text"  v-model="field5"   placeholder="Введите 5 строку" v-show="note5">
-    
-                                            
-            </div>
-            <div class="plus_minus_p">
-            <p>Добавить или убавить поле для заметки</p>
-            </div>
-                <div class="minus_plus">
-                     
-                       <p class="plus">
-                            <button type='button' @click="addField"> + </button>
-                       </p>
-                       
-                       <p class="minus">
-                            <button type='button' @click="removeField"> - </button>
-                       </p>
+    <section>
+    <a href="#openModal" class="btn btnModal">Создать карточку</a>
+    <div id="openModal" class="modal">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h3 class="modal-title">Название</h3>
+            <a href="#close" title="Close" class="close">×</a>
+          </div>
+          <div class="modal-body">    
+        <div class="addForm">
+            <form @submit.prevent="onSubmit">
+                <div class="form__control">
+                    <div class="form__name field">
+                        <input id="point" required v-model="name" type="text" placeholder="Название">
+                    </div>
+                <div class="field">
+                    <textarea required id="point" v-model="description" placeholder="Описание"> </textarea>
                 </div>
-                
-                <div>                    
-                    <p class="sub">
-                            <input type="submit" value="Отправить"> 
-                    </p> 
+                <div class="field">
+                    <input required type="date" id="point" v-model="deadline">
                 </div>
-          </form>
+                <button type="submit" class="btn" @click="persist" >Добавить</button>
+                </div>
+            </form>
+        </div>
+              </div>
+        </div>
+      </div>
+    </div>
+    </section>
  `,
 
     data() {
         return {
-            note4: false,
-            note5: false,
-            name: "",
-            field1: "",
-            field2: "",
-            field3: "",
-            field4: "",
-            field5: "",
+            name: null,
+            description: null,
+            date: null,
+            deadline: null
 
 
         }
     },
     methods: {
         onSubmit() {
-            if (this.name && this.field1 && this.field2 && this.field3 && (!this.note4 || this.field4) && (!this.note5 || this.field5)) {
-                let FieldsField = {
-                    name: this.name,
-                    data: null,
-                    status: 0,
-                    errors: [],
-                }
-                eventBus.$emit('fields1-submitted', FieldsField)
-                this.name = null
+            let card = {
+                name: this.name,
+                description: this.description,
+                date: new Date().toLocaleDateString().split(".").reverse().join("-"),
+                deadline: this.deadline,
+                reason: [],
+                transfer: false,
+                edit: false,
+                editDate: null,
+                period: true
             }
-        },
-    },
+            eventBus.$emit('addColumn_1', card)
+            this.name = null
+            this.description = null
+            this.date = null
+            this.deadline = null
+            },
+        persist() {
+            localStorage.name = this.name;
+            localStorage.description = this.description;
+            localStorage.deadline = this.deadline;
+        }
 
-    computed: {},
+        },
+
+    computed: {
+
+    },
 
     props: {
         note_list1:{
@@ -146,6 +159,11 @@ Vue.component('add-notes', {
             required: false,
 
         },
+    },
+    mounted() {
+        if(localStorage.name) this.name = localStorage.name;
+        if(localStorage.description) this.description = localStorage.description;
+        if(localStorage.deadline) this.deadline = localStorage.deadline;
     }
 })
 
